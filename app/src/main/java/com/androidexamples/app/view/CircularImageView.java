2d7376f877504842696ca0fb622cff4b7e15d4c9
Marcos Vinícius <marcosvbras@gmail.com>
2017-02-21 00:20:33 -0300

    Alterações na imagem circular

--- app/src/main/java/com/androidexamples/app/view/CircularImageView.java
+++ app/src/main/java/com/androidexamples/app/view/CircularImageView.java
@@ -1,75 +1,44 @@
 package com.androidexamples.app.view;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Bitmap;
-import android.graphics.BitmapShader;
+import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Shader;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
-import com.androidexamples.app.R;
-
 /**
- * Created by marcos on 07/01/2017.
+ * Created by marcosvbras on 19/02/2017.
  */
 
 public class CircularImageView extends ImageView {
 
     private int borderWidth;
-    private int canvasSize;
-    private Bitmap image;
-    private Paint paint;
     private Paint paintBorder;
 
-    public CircularImageView(final Context context) {
-        this(context, null);
-        if (this.isInEditMode()) {
+    public CircularImageView(Context context) {
+        super(context);
+
+        if (this.isInEditMode())
             return;
-        }
     }
 
     public CircularImageView(Context context, AttributeSet attrs) {
-        this(context, attrs, R.attr.circularImageViewStyle);
-        if (this.isInEditMode()) {
+        super(context, attrs);
+
+        if (this.isInEditMode())
             return;
-        }
     }
 
     public CircularImageView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-
-        if (!this.isInEditMode()) {
-            paint = new Paint();
-            paint.setAntiAlias(true);
-            paintBorder = new Paint();
-            paintBorder.setAntiAlias(true);
-            // load the styled attributes and set their properties
-            TypedArray attributes = context.obtainStyledAttributes(attrs,
-                    R.styleable.CircularImageView, defStyle, 0);
-            if (attributes.getBoolean(R.styleable.CircularImageView_border,
-                    true)) {
-                int defaultBorderSize = (int) (0 * getContext().getResources()
-                        .getDisplayMetrics().density + 0.5f);
-                setBorderWidth(attributes.getDimensionPixelOffset(
-                        R.styleable.CircularImageView_border_width,
-                        defaultBorderSize));
-                setBorderColor(attributes
-                        .getColor(R.styleable.CircularImageView_border_color,
-                                Color.WHITE));
-            }
-            if (attributes.getBoolean(R.styleable.CircularImageView_shadow,
-                    false))
-                addShadow();
-        } else {
-            return;
-        }
     }
 
     public void setBorderWidth(int borderWidth) {
@@ -81,6 +50,7 @@ public class CircularImageView extends ImageView {
     public void setBorderColor(int borderColor) {
         if (paintBorder != null)
             paintBorder.setColor(borderColor);
+
         this.invalidate();
     }
 
@@ -90,88 +60,60 @@ public class CircularImageView extends ImageView {
     }
 
     @Override
-    @SuppressLint("DrawAllocation")
-    public void onDraw(Canvas canvas) {
-        // load the bitmap
+    protected void onDraw(Canvas canvas) {
+        Bitmap bitmap = drawableToBitmap(getDrawable());
 
-        if (!this.isInEditMode()) {
-            image = drawableToBitmap(getDrawable());
-            // init shader
-            if (image != null) {
-                canvasSize = canvas.getWidth();
-                if (canvas.getHeight() < canvasSize)
-                    canvasSize = canvas.getHeight();
-                BitmapShader shader = new BitmapShader(Bitmap.createScaledBitmap(
-                        image, canvasSize, canvasSize, false),
-                        Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
-                paint.setShader(shader);
-                // circleCenter is the x or y of the view's center
-                // radius is the radius in pixels of the cirle to be drawn
-                // paint contains the shader that will texture the shape
-                int circleCenter = (canvasSize - (borderWidth * 2)) / 2;
-                canvas.drawCircle(circleCenter + borderWidth, circleCenter
-                        + borderWidth, ((canvasSize - (borderWidth * 2)) / 2)
-                        + borderWidth - 4.0f, paintBorder);
-                canvas.drawCircle(circleCenter + borderWidth, circleCenter
-                                + borderWidth,
-                        ((canvasSize - (borderWidth * 2)) / 2) - 4.0f, paint);
-            }
-        }
+        if(bitmap == null)
+            return;
+
+        int width = getWidth();
+        int height = getHeight();
+
+        Bitmap roundBitmap = getCroppedBitmap(bitmap, width);
+        canvas.drawBitmap(roundBitmap, 0, 0, null);
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int width = measureWidth(widthMeasureSpec);
-        int height = measureHeight(heightMeasureSpec);
-        setMeasuredDimension(width, height);
-    }
+    public static Bitmap getCroppedBitmap(Bitmap originalBitmap, int radius) {
+        Bitmap sbmp;
 
-    private int measureWidth(int measureSpec) {
-
-        int result = 0;
-        int specMode = MeasureSpec.getMode(measureSpec);
-        int specSize = MeasureSpec.getSize(measureSpec);
-        if (specMode == MeasureSpec.EXACTLY) {
-            // The parent has determined an exact size for the child.
-            result = specSize;
-        } else if (specMode == MeasureSpec.AT_MOST) {
-            // The child can be as large as it wants up to the specified size.
-            result = specSize;
+        if (originalBitmap.getWidth() != radius || originalBitmap.getHeight() != radius) {
+            float smallest = Math.min(originalBitmap.getWidth(), originalBitmap.getHeight());
+            float factor = smallest / radius;
+            sbmp = Bitmap.createScaledBitmap(originalBitmap, (int)(originalBitmap.getWidth() / factor), (int)(originalBitmap.getHeight() / factor), false);
         } else {
-            // The parent has not imposed any constraint on the child.
-            result = canvasSize;
+            sbmp = originalBitmap;
         }
-        return result;
+
+        Bitmap output = Bitmap.createBitmap(radius, radius, Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xffa19774;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, radius, radius);
+
+        Rect rectSrc = new Rect(sbmp.getWidth()/2 - radius/2, sbmp.getHeight()/2 - radius/2,
+                sbmp.getWidth()/2 + radius/2, sbmp.getHeight()/2 + radius/2
+        );
+
+        paint.setAntiAlias(true);
+        paint.setFilterBitmap(true);
+        paint.setDither(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(Color.parseColor("#BAB399"));
+        canvas.drawCircle(radius / 2 + 0.7f, radius / 2 + 0.7f, radius / 2 + 0.1f, paint);
+        paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+        canvas.drawBitmap(sbmp, rectSrc, rect, paint);
+
+        return output;
     }
 
-    private int measureHeight(int measureSpecHeight) {
-
-        int result = 0;
-        int specMode = MeasureSpec.getMode(measureSpecHeight);
-        int specSize = MeasureSpec.getSize(measureSpecHeight);
-        if (specMode == MeasureSpec.EXACTLY) {
-            // We were told how big to be
-            result = specSize;
-        } else if (specMode == MeasureSpec.AT_MOST) {
-            // The child can be as large as it wants up to the specified size.
-            result = specSize;
-        } else {
-            // Measure the text (beware: ascent is a negative number)
-            result = canvasSize;
-        }
-        return (result + 2);
-    }
-
-    public Bitmap drawableToBitmap(Drawable drawable) {
-
-
-        if (drawable == null) {
+    private Bitmap drawableToBitmap(Drawable drawable) {
+        if (drawable == null)
             return null;
-        } else if (drawable instanceof BitmapDrawable) {
+        else if (drawable instanceof BitmapDrawable)
             return ((BitmapDrawable) drawable).getBitmap();
-        }
-        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
-                drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Config.ARGB_8888);
         Canvas canvas = new Canvas(bitmap);
         drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
         drawable.draw(canvas);
